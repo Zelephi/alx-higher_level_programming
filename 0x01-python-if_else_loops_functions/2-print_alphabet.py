59353428a19fa79ef0a8c73f9e6dd7e1a997444f
Zelephi <ephayele@gmail.com>
2022-09-21 00:09:41 +0300

    a program that prints the ASCII alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-alpha = 97
-for alpha in range(97, 123):
-    print("{}".format(chr(alpha)), end='')
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
