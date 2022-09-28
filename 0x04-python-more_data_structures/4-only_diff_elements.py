621c2060472d4dd5f4ee8c1a05a74967701422ed
Zelephi <ephayele@gmail.com>
2022-09-28 21:49:04 +0300

    Return a set of all elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # 4-only_diff_elements.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+# Return a set of all elements present in only one set.
 
 
 def only_diff_elements(set_1, set_2):
-    """Return a set of all elements present in only one set."""
     return (set_1 ^ set_2)
