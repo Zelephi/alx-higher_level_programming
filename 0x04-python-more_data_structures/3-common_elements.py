6fcbd620fab067eefb6738655a651ee74944e3c9
Zelephi <ephayele@gmail.com>
2022-09-28 21:46:14 +0300

    Returns a set of common elements in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # 3-common_elements.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+# Return a set of common elements in two sets.
 
 
 def common_elements(set_1, set_2):
-    """Return a set of common elements in two sets."""
     return (set_1 & set_2)
