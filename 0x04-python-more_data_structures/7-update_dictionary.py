511bb32c07aa633cf409ec975abc738dea020de2
Zelephi <ephayele@gmail.com>
2022-09-28 21:59:05 +0300

    Replace or add key or value pairs in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # 7-update_dictionary.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+# Replace or add key or value pairs in a dictionary.
 
 
 def update_dictionary(a_dictionary, key, value):
-    """Replace or add key/value pairs in a dictionary."""
     a_dictionary[key] = value
     return (a_dictionary)
